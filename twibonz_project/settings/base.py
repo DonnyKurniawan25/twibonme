bf87e10222a6126de28dea2952c53e9073eb53f5
DonnyKurniawan25 <donyacm25@gmail.com>
2025-11-24 22:46:11 +0800

    update deploy server

--- twibonz_project/settings/base.py
+++ twibonz_project/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'twibonz_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
